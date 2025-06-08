39950caa3bcc9cdd7a3459752ea683acaf209af2
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-06-08 13:03:39 +0300

    Rewrite gitignore to not ignore typescript files + Product class and ProductService implementation

---
+++ front end/angular-ecommerce/src/app/app-module.ts
@@ -0,0 +1,27 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { ProductList } from './component/product-list/product-list';
+import { ProductService } from './services/product';
+import { provideHttpClient } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    App,
+    ProductList
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideHttpClient(),
+    ProductService,
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
