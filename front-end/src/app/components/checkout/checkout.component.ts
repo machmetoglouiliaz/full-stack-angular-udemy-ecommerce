6146c359221b7f3572766cecf2818b9cfd994896
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-06-18 20:28:10 +0300

    feat: add checkout form using FormGroup for form control management

--- front-end/src/app/components/checkout/checkout.component.ts
+++ front-end/src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OnInit } from '../../../../node_modules/@angular/core/index';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -6,6 +8,59 @@ import { Component } from '@angular/core';
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
 
+  checkoutFormGroup!: FormGroup;
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  constructor(private formBuilder: FormBuilder) { }
+
+
+  ngOnInit(): void {
+
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipcode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipcode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      })
+    });
+
+  }
+
+  onSubmit() {
+    console.log(this.checkoutFormGroup.get('customer')!.value);
+  }
+
+  copyShippingAddressToBillingAddress(event) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress'].setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    }
+    else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+  }
 }
