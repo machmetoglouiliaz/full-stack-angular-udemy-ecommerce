743e8507c292af49b5042b689691ca75ffa82a60
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-06-20 13:09:04 +0300

    feat: add validation to customer form group

--- front-end/src/app/components/checkout/checkout.component.ts
+++ front-end/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { OnInit } from '../../../../node_modules/@angular/core/index';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CheckoutFormService } from '../../services/checkout-form.service';
 import { Country } from '../../common/country';
 import { State } from '../../common/state';
@@ -34,9 +34,10 @@ export class CheckoutComponent implements OnInit {
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('', 
+          [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -69,6 +70,11 @@ export class CheckoutComponent implements OnInit {
   }
 
   onSubmit() {
+
+    if(this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+
     console.log('Handling purchase submit button')
     console.log("Customer:" + this.checkoutFormGroup.get('customer')!.value);
     console.log("Shipping Address:");
@@ -91,6 +97,10 @@ export class CheckoutComponent implements OnInit {
       this.billingAddressStates = [];
     }
   }
+
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName');}
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName');}
+  get email() { return this.checkoutFormGroup.get('customer.email');}
 
   getMonths() {
 
