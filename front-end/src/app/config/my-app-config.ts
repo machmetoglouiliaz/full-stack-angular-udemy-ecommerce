54458d585868cb5105a4a9fd54679e9a8cca817b
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-06-22 12:38:51 +0300

    feat: upgrade Angular version and integrate Okta authentication
    - Updated Angular to the latest version
    - Installed required Okta dependencies
    - Added Okta configuration file for authentication setup

---
+++ front-end/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default  {
+
+    oidc: {
+        clientId:'0oap9y34c3HEWG3O65d7',
+        issuer: 'https://dev-10789014.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
