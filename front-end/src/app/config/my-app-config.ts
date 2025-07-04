8062fee159747035c4d943572a1c81813e7c32ec
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-07-04 19:08:07 +0300

    feat(security): implement SSL support refactor(config): switch app to HTTPS

--- front-end/src/app/config/my-app-config.ts
+++ front-end/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default  {
     oidc: {
         clientId:'0oap9y34c3HEWG3O65d7',
         issuer: 'https://dev-10789014.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
