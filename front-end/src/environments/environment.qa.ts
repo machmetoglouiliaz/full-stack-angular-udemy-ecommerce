0456477bd0173d748c1fac1acf5de7c8ce6ce1a8
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-07-06 00:34:13 +0300

    refactor: small environment arrangements

--- front-end/src/environments/environment.qa.ts
+++ front-end/src/environments/environment.qa.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    demoAngularEcommerceUrl: "https://localhost:9898/api"
+    demoAngularEcommerceUrl: "https://localhost:9898/api",
+    stripePublishableKey: "pk_test_51RhWUuQeDRPEqNgePDJD9V77NvQOlTpcSBOxOfzFgBuDzXrSoL0300zfGLatp7l9Jubm3Eu1nDo5jY91maoqw1YY00zyYepqmw"
+
 };
